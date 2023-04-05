469cf0fe065c3ff8ac41d8f6704d83933baddace
Liam Clink <clink.6@osu.edu>
2023-04-05 18:15:47 -0400

    Made shutter wait for motion to stop

--- RasterBlaster.py
+++ RasterBlaster.py
@@ -21,7 +21,7 @@ mot1 = mg.motor()
 mot1.comset(1)
 mot1.unpark(1)
 
-rasterLength = 3000 #length of each raster unit - microns
+rasterLength = 10000 #length of each raster unit - microns
 speed = 2000 # speed in um/s (max is 2500 um/s)
 rasterTime = rasterLength/speed #time seconds to make each raster
 print(f"The raster time is: {rasterTime}")
@@ -34,6 +34,12 @@ if speed >= 2500:
 mot1.setspeed(1,speed)
 shutter.setShutterOpen() #open shutter
 mot1.move(1,rasterLength*direction) #move
+while True:
+    blank, status = mot1.readstat(motornum=1)
+    if status == 'Motor is moving':
+        continue
+    else:
+        break
 shutter.setShutterClose() #close shutter
-#move a little more and repeat
+mot1.move(1,-rasterLength*direction) #return to start
 mot1.park(1)
